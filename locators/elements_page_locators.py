972db5411612110dd61bdb816a182414db673b3b
Denis Tadzhibaev <incinerator88@mail.ru>
2023-04-16 23:01:08 +0300

    fix: chance locators name

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -2,11 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class TextBoxPageLocators:
-
     FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
     EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
-    CURRENT_ADRES = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
-    PERMANENT_ADRES = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
     SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
 
     CREATED_FULL_NAME = (By.CSS_SELECTOR, "#output #name")
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -13,8 +13,8 @@ class TextBoxPage(BasePage):
         permanent_address = info.permanent_address
         self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
         self.element_is_visible(self.locators.EMAIL).send_keys(email)
-        self.element_is_visible(self.locators.CURRENT_ADRES).send_keys(current_address)
-        self.element_is_visible(self.locators.PERMANENT_ADRES).send_keys(permanent_address)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
         return full_name, email, current_address, permanent_address
 
